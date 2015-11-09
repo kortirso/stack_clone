515e33bd334e6826fb803e6826c351572ef8479c
Bogdanov Anton <kortirso@gmail.com>
2015-11-09 18:33:45 +0300

    add Answer model, add tests for answer model

---
+++ app/models/answer.rb
@@ -0,0 +1,5 @@
+class Answer < ActiveRecord::Base
+	validates :body, :question_id, presence: true
+
+	belongs_to :question
+end
---
+++ spec/models/answer_spec.rb
@@ -0,0 +1,4 @@
+RSpec.describe Answer, type: :model do
+	it { should validate_presence_of :body }
+	it { should validate_presence_of :question_id }
+end
