f3726aa99c3151de4aee26c4128175fdcde7089e
Bogdanov Anton <kortirso@gmail.com>
2015-11-09 18:26:35 +0300

    add spec for question model

--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -1,3 +1,4 @@
 RSpec.describe Question, type: :model do
-	pending "add some examples to (or delete) #{__FILE__}"
+	it { should validate_presence_of :title }
+	it { should validate_presence_of :body }
 end
