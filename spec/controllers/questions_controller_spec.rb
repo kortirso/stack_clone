75c60c119d6f80756cd15f8fa1add8494d1f5f71
Bogdanov Anton <kortirso@gmail.com>
2015-11-10 20:57:58 +0300

    add test of show action in questions_controller

--- spec/controllers/questions_controller_spec.rb
+++ spec/controllers/questions_controller_spec.rb
@@ -13,4 +13,19 @@ RSpec.describe QuestionsController, type: :controller do
             expect(response).to render_template :index
         end
     end
+
+    describe 'GET #show' do
+        let(:question) { create :question }
+        before do
+            get :show, id: question
+        end
+
+        it 'assigns the requested question to @question' do
+            expect(assigns(:question)).to eq question
+        end
+
+        it 'renders show view' do
+            expect(response).to render_template :show
+        end
+    end
 end
