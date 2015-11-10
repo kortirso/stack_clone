b80bb5005f2d2564fb5b83f74d54410018ce3455
Bogdanov Anton <kortirso@gmail.com>
2015-11-10 20:40:24 +0300

    add specs to question controller

--- spec/controllers/questions_controller_spec.rb
+++ spec/controllers/questions_controller_spec.rb
@@ -1,3 +1,17 @@
 RSpec.describe QuestionsController, type: :controller do
+    describe 'GET #index' do
+        it 'collect an array of all questions' do
+            question_1 = create(:question)
+            question_2 = create(:question)
 
+            get :index
+
+            expect(assigns(:questions)).to match_array([question1, question2])
+        end
+        it 'renders index view' do
+            get :index
+
+            expect(response).to render_template :index
+        end
+    end
 end
