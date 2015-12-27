1ef3d4169e77aec85b45e6b2040a2169b475cf64
Bogdanov Anton <kortirso@gmail.com>
2015-12-27 21:05:43 +0300

    add includes method

--- app/jobs/send_to_question_subscribers_job.rb
+++ app/jobs/send_to_question_subscribers_job.rb
@@ -2,7 +2,7 @@ class SendToQuestionSubscribersJob < ActiveJob::Base
     queue_as :default
 
     def perform(answer)
-        answer.question.subscribes.each do |subscribe|
+        answer.question.subscribes.includes(:user).each do |subscribe|
             Mailer.notify_subscribers(subscribe.user, answer).deliver_later
         end
     end
